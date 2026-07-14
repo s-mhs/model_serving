a32ef04ba337cb4eef0db7262aa4517a097881ee
s-mhs <smichailidis@covariance.gr>
2026-07-14 12:09:37 +0300

    simple response post for local qwen model

--- main.py
+++ main.py
@@ -1,5 +1,26 @@
+import requests
+
+def get_response(prompt: str):
+    return requests.post(
+        "http://localhost:11434/api/chat",
+        json={
+            "model": "qwen3-v1:8b",
+            "stream": False,
+            "messages": [
+                {
+                    "role": "user",
+                    "content": prompt
+                }
+            ]
+        },
+    )
+
 def main() -> int:
-    print("hello, artist")
+    response = get_response("what's your name?")
+    response.raise_for_status()
+    
+    print(response.json()["messages"]["content"])
+    
     return 0 
 
 if __name__ == "__main__":
